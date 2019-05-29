f2989c30580ef4ab9e58b38fc1bd137407ac0759
Your Name <you@example.com>
2019-05-29 18:42:09 +0000

    fixing the header bug

--- aws-linear-estimator/sklearn_fd_featurizer.py
+++ aws-linear-estimator/sklearn_fd_featurizer.py
@@ -22,13 +22,21 @@ from sklearn.preprocessing import Binarizer, StandardScaler, OneHotEncoder
 from sagemaker_containers.beta.framework import (content_types, encoders, env, modules, transformer, worker)
 
 # Since we get a headerless CSV file we specify the column names here.
-# feature_columns_names = data.loc[:, data.columns != 'Class'].columns.tolist()
 
-# label_column = 'Class'
+# Since we get a headerless CSV file we specify the column names here.
+feature_columns_names = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10','V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount']
 
-# feature_columns_dtype = data.loc[:, data.columns != 'Class'].dtypes.to_dict()
+label_column = 'Class'
 
-# label_column_dtype = {'Class': np.int64} # +1.5 gives the age in years
+feature_columns_dtype = {'Time': np.float64,
+    'V1': np.float64,'V2': np.float64,'V3': np.float64,'V4': np.float64,'V5': np.float64,'V6': np.float64,
+    'V7': np.float64,'V8': np.float64,'V9': np.float64,'V10': np.float64,'V11': np.float64,'V12': np.float64,
+    'V13': np.float64,'V14': np.float64,'V15': np.float64,'V16': np.float64,'V17': np.float64,'V18': np.float64,
+    'V19': np.float64,'V20': np.float64,'V21': np.float64,'V122': np.float64,
+    'V23': np.float64,'V24': np.float64,'V25': np.float64,'V26': np.float64,'V27': np.float64,'V28': np.float64,
+    'Amount': np.float64}
+
+label_column_dtype = {'Class': np.float64} 
 
 def merge_two_dicts(x, y):
     z = x.copy()   # start with x's keys and values
@@ -54,15 +62,13 @@ if __name__ == '__main__':
                           'the data specification in S3 was incorrectly specified or the role specified\n' +
                           'does not have permission to access the data.').format(args.train, "train"))
     
-#     raw_data = [ pd.read_csv(
-#         file, 
-#         header=None, 
-#         names=feature_columns_names + [label_column],
-#         dtype=merge_two_dicts(feature_columns_dtype, label_column_dtype)) for file in input_files ]
-#     concat_data = pd.concat(raw_data)
-    print(input_files)
-    raw_data = [pd.read_csv(file) for file in input_files]
+    raw_data = [ pd.read_csv(
+        file, 
+        header=None, 
+        names=feature_columns_names + [label_column],
+        dtype=merge_two_dicts(feature_columns_dtype, label_column_dtype)) for file in input_files ]
     concat_data = pd.concat(raw_data)
+
     # This section is adapted from the scikit-learn example of using preprocessing pipelines:
     #
     # https://scikit-learn.org/stable/auto_examples/compose/plot_column_transformer_mixed_types.html
@@ -79,7 +85,7 @@ if __name__ == '__main__':
     # Categorical Features:
     # - sex: categories encoded as strings {'M', 'F', 'I'} where 'I' is Infant
         
-    feature_columns_names = concat_data.loc[:, concat_data.columns != 'Class'].columns.tolist()
+    
     numeric_features = list(feature_columns_names)
     numeric_transformer = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='median')),
@@ -94,34 +100,34 @@ if __name__ == '__main__':
     print("saved model!")
     
     
+
 def input_fn(input_data, content_type):
+    print("calling input_fn")
     """Parse input data payload
-    
     
     We currently only take csv input. Since we need to process both labelled
     and unlabelled data we first determine whether the label column is present
     by looking at how many columns were provided.
     """
-
-    feature_columns_names= ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10','V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount']
     if content_type == 'text/csv':
         # Read the raw input data as CSV.
-        df = pd.read_csv(StringIO(input_data), header = None)
-
-        #df.columns = feature_columns_names
-        if len(df.columns) > len(feature_columns_names):
-            # This is a labelled example, includes the class label
-            df.columns = feature_columns_names + ['Class']
+        df = pd.read_csv(StringIO(input_data), 
+                         header=None)
+        
+        if len(df.columns) == len(feature_columns_names) + 1:
+            # This is a labelled example, includes the ring label
+            df.columns = feature_columns_names + [label_column]
         elif len(df.columns) == len(feature_columns_names):
             # This is an unlabelled example.
             df.columns = feature_columns_names
-  
+        print(df.columns)   
+        print(df.head())   
         return df
     else:
         raise ValueError("{} not supported by script!".format(content_type))
-        
 
 def output_fn(prediction, accept):
+    print("calling output_fn")
     """Format prediction output
     
     The default accept/content-type between containers for serial inference is JSON.
@@ -143,6 +149,7 @@ def output_fn(prediction, accept):
 
 
 def predict_fn(input_data, model):
+    print("calling predict_fn")
     """Preprocess input data
     
     We implement this because the default predict_fn uses .predict(), but our model is a preprocessor
@@ -152,7 +159,7 @@ def predict_fn(input_data, model):
     
         rest of features either one hot encoded or standardized
     """
-
+    print(input_data.head())
     if 'Class' in input_data:
         
         features = model.transform(input_data.loc[:, input_data.columns != 'Class'])
@@ -166,6 +173,7 @@ def predict_fn(input_data, model):
     
 
 def model_fn(model_dir):
+    print("calling model_fn")
     """Deserialize fitted model
     """
     preprocessor = joblib.load(os.path.join(model_dir, "model.joblib"))
